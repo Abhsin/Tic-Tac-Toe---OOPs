80b67bab786576c312c57f76aefb3098f39fcae4
Abhishek Singh <44022825+Abhsin@users.noreply.github.com>
2023-11-15 21:47:40 +0000

    Added OOPS

--- cli.py
+++ cli.py
@@ -1,66 +1,116 @@
-from logic import check_winner
-def get_empty_board():
-    """
-    row_1 = ['O', 'O', 'O']
-    row_2 = ['O', 'O', 'O']
-    row_3 = ['O', 'O', 'O']
-
-    board = [row_1, row_2, row_3]
-    """
-    return [
-        [None, None, None],
-        [None, None, None],
-        [None, None, None],
-    ]
+import random
 
 
+class Board:
+    def __init__(self):
+        self.board = [" " for _ in range(9)]  # 3x3 Tic Tac Toe board
 
-def get_player_input(current_player):
-    """"
-    input : row, col
-    return:
-        row -> int -> the index of Row 
-        col -> int -> the index of Column
-    """
-    prompt = f"player {current_player}, Please inpute your move, e.g. row,col \n"
-    player_input = input(prompt) # This is a string 
-    # 1,1.split (",") --> ["1","1"]
-    row_col_list = player_input.split(',')
-    row,col = [int(x) for x in row_col_list]
-    return row,col
+    def print_board(self):
+        for i in range(3):
+            print("|".join(self.board[i * 3 : (i + 1) * 3]))
+            if i < 2:
+                print("-----")
 
-def switch_player(current_player):
-    if current_player == 'X':
-        return 'O'
-    return 'X'
+    def make_move(self, position, symbol):
+        if self.is_valid_move(position):
+            self.board[position] = symbol
+            return True
+        return False
+
+    def is_valid_move(self, position):
+        if 0 <= position <= 8:
+            return self.board[position] == " "
+        return False
+
+    def check_winner(self, symbol):
+        # Check all winning conditions
+        for i in range(3):
+            # Check rows and columns
+            if (
+                self.board[i * 3]
+                == self.board[i * 3 + 1]
+                == self.board[i * 3 + 2]
+                == symbol
+            ) or (self.board[i] == self.board[i + 3] == self.board[i + 6] == symbol):
+                return True
+        # Check diagonals
+        if (self.board[0] == self.board[4] == self.board[8] == symbol) or (
+            self.board[2] == self.board[4] == self.board[6] == symbol
+        ):
+            return True
+        return False
+
+    def is_full(self):
+        return " " not in self.board
 
 
-def print_board(board):
-    for row in board: 
-        print(row)# Print will the variable in a new line
+class Player:
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+    def make_move(self, board):
+        pass
 
 
-if __name__ == '__main__':
-    #get a empty board 
-    current_player = 'X'
-    board= get_empty_board()
-    winner = None
-
-    while winner is None:
-        print_board(board)
-    #ask user input
+class HumanPlayer(Player):
+    def make_move(self, board):
         try:
-            row, col = get_player_input(current_player) # ask user input
+            position = int(input(f"Player {self.symbol}, enter your move (0-8): "))
         except ValueError:
-            print("Invalid input, try again\n")
-            continue
-    # mark the board 
-        board[row][col] = current_player
-        winner = check_winner(board)
-        if winner != None:
-            print(f"Winner is {current_player}")
-        current_player = switch_player(current_player)
-print_board(board)
+            return False
+        return board.make_move(position, self.symbol)
 
 
+class BotPlayer(Player):
+    def make_move(self, board):
+        valid_moves = [i for i in range(9) if board.is_valid_move(i)]
+        print(f"Valid moves: {valid_moves}")
+        position = random.choice(valid_moves)
+        print(f"BotPlayer {self.symbol} chooses position {position}")
+        return board.make_move(position, self.symbol)
 
+
+class Game:
+    def __init__(self, player1, player2):
+        self.board = Board()
+        self.player1 = player1
+        self.player2 = player2
+        self.current_player = self.player1
+
+    def switch_player(self):
+        self.current_player = (
+            self.player1 if self.current_player == self.player2 else self.player2
+        )
+
+    def play(self):
+        while True:
+            self.board.print_board()
+            if not self.current_player.make_move(self.board):
+                print("Invalid move, try again.")
+                continue
+
+            if self.board.check_winner(self.current_player.symbol):
+                self.board.print_board()
+                print(f"Player {self.current_player.symbol} wins!")
+                break
+
+            if self.board.is_full():
+                self.board.print_board()
+                print("It's a tie!")
+                break
+
+            self.switch_player()
+
+
+# To play the game
+num_human_players = input("How many human players? (1/2): ")
+player1 = HumanPlayer("X")
+player2 = BotPlayer("O")
+if num_human_players == "1":
+    player2 = BotPlayer("O")
+elif num_human_players == "2":
+    player2 = HumanPlayer("O")
+else:
+    print("Invalid input, defaulting to 1 human player.")
+game = Game(player1, player2)
+game.play()
